397f24b39e5c8fc7013229f76fb65d1229427cff
Hasan1Aref <Hasan.aref.eng@gmail.com>
2021-03-21 20:21:20 +0200

    Creating table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,15 @@ SELECT Points from students  WHERE name='Alex';
 INSERT INTO students (ID,NAME,Age,Gender,Points) VALUES (7,'Hasan',23,'M',500);
 UPDATE students SET Points=350   where name='Basma';
 UPDATE students SET Points=150   where name='Alex';
+
+CREATE TABLE "graduates " (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+insert into graduates(ID,Name,Age,Gender,Points,Graduation)VALUES(4,'Layal',18,'F',350,'3/20/2021');
+DELETE  FROM students WHERE NAME = 'Layal';
