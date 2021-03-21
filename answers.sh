c3221b8fe8ca5546ac4ac2ae75d91999d47035e9
Hasan1Aref <Hasan.aref.eng@gmail.com>
2021-03-21 20:23:39 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -17,3 +17,16 @@ CREATE TABLE "graduates " (
 );
 insert into graduates(ID,Name,Age,Gender,Points,Graduation)VALUES(4,'Layal',18,'F',350,'3/20/2021');
 DELETE  FROM students WHERE NAME = 'Layal';
+
+SELECT e.name,e.Company,c.date
+FROM employees e 
+INNER JOIN companies c
+ON e.Company = c.name;
+SELECT e.name
+FROM employees e 
+INNER JOIN companies c
+ON e.Company = c.name AND date<2000;
+SELECT c.name
+FROM employees e 
+INNER JOIN companies c
+ON e.Company = c.name AND Role='Graphic Designer';
